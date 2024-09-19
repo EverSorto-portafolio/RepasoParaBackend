f1051ad0842e4e804c57d723f68a7fc7cc0ffe07
EverSorto-portafolio <everalfredo.sorto@gmail.com>
2024-09-19 09:14:25 -0600

    Se crea el proyecto para aprender lo basico de c#

---
+++ RetrolaimentacionC#/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using System.Security.Cryptography.X509Certificates;
+
+Venta ventas = new Venta(12);
+VentaconImpuestos ventas1 = new (154);
+var mensage2 = ventas1.impuesto(154); 
+
+var message = ventas1.GetInfo();
+Console.WriteLine(message + " impuestos total son " + mensage2 );
+
+
+class Venta {
+
+    public decimal Total { get; set; }
+
+    public Venta(decimal total) {
+        this.Total = total;
+    }
+
+    public string GetInfo() { 
+        return "El total es " + Total;
+    }
+    private string _SoloEnLaClase()
+    {
+        return "El total es " + Total;
+    }
+
+    protected String SoloHeredados()
+    {
+        return "El total es " + Total;
+    }
+}
+class VentaconImpuestos : Venta 
+{
+    public VentaconImpuestos(decimal total) : base(total)
+    { }
+
+    public decimal impuesto(decimal impuesto) { 
+      var total =  this.Total *  impuesto;
+        return total;
+    }
+}
