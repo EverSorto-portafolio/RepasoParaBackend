ffc6eb846d5263cb01ec3299aeea2eda9fedcd05
EverSorto-portafolio <everalfredo.sorto@gmail.com>
2024-09-19 23:02:40 -0600

    guia de refuerzo, constructor, herencia, clase, sobre escritura, sobre carga de metodos

--- RetrolaimentacionC#/Program.cs
+++ RetrolaimentacionC#/Program.cs
@@ -2,12 +2,12 @@
 using System.Security.Cryptography.X509Certificates;
 
 Venta ventas = new Venta(12);
-VentaconImpuestos ventas1 = new (154);
+VentaconImpuestos ventas1 = new (154, 15.65m);
 var mensage2 = ventas1.impuesto(154); 
 
 var message = ventas1.GetInfo();
-Console.WriteLine(message + " impuestos total son " + mensage2 );
-
+Console.WriteLine(message + "  impuestos total son " + mensage2 );
+Console.WriteLine(ventas1.getInfoCliente("Ever Sorto"));
 
 class Venta {
 
@@ -29,14 +29,33 @@ class Venta {
     {
         return "El total es " + Total;
     }
+
+    public virtual string getInfoCliente() {
+        return "El cliente se llama Carlor";
+    }
+
 }
 class VentaconImpuestos : Venta 
 {
-    public VentaconImpuestos(decimal total) : base(total)
-    { }
+
+    decimal porcentaje;
+    public VentaconImpuestos(decimal total, decimal porcentaje) : base(total){
+        this.porcentaje = porcentaje;
+    
+    }
 
     public decimal impuesto(decimal impuesto) { 
       var total =  this.Total *  impuesto;
         return total;
+    }
+
+    public override string getInfoCliente()
+    {
+            return "El cliete se llama Juan " + porcentaje ;
+    }
+
+    public  string getInfoCliente( string cliente)
+    {
+        return cliente;
     }
 }
